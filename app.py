334fa8df321d9ed7f7aa2e3d1c4c388702d48038
Aditya Acharya <aacha002@ucr.edu>
2019-04-26 22:07:10 -0700

    twilio set up

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 
 from flask import Flask, render_template,request
 
+from twilio.rest import Client
+
 
 app = Flask(__name__)
 
 
 
-@app.route("/")
+account = "" #do not commit lol
+token = "" # do not commit lol
+client = Client(account,token)
+
+@app.route("/", methods=['GET','POST'])
 def main():
-            return render_template("index.html")
+
+    if request.method == 'POST':
+            results = request.form
+            #print(results.get(sendtext,-1))
+
+            text_message = results.get('message',-1)
+    
+            message = client.messages.create(to = "+15104742763", from_ = "+19258923648", body = text_message) 
+
+
+    return render_template("index.html")
 
 
 
