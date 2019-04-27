8d4fbe295c63bfc29648597eb5a331bd31ae52ab
Joshua <jsun034@ucr.edu>
2019-04-27 00:16:49 -0700

    some style

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Home.css';
 
 export default class Home extends Component {
   render() {
@@ -9,6 +10,23 @@ export default class Home extends Component {
             <div className="panel-container">
               <div className="container-fluid">
                 <p>panel</p>
+                
+                <h1>Carpuul</h1>
+				<p>Carpuuling made right for everyone, everywhere.</p>
+
+				<div class="container">
+				  <div class="row">
+				    <div class="col">
+						<button type="button" class="btn btn-light">1st location</button>
+						<button type="button" class="btn btn-light">2nd location</button>
+				    </div>
+				    <div class="col">
+						<button type="button" class="btn btn-light">1st phone number</button>
+						<button type="button" class="btn btn-light">2nd phone number</button>
+				    </div>
+				  </div>
+				</div>
+				<button type="button" class="btn btn-light plusButton">+</button>
               </div>
             </div>
           </div>
