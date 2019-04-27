9277d3b56a15231ebae6c4b160d9f4be02b7dff4
Aditya Acharya <aacha002@ucr.edu>
2019-04-26 21:19:49 -0700

    Initial commit MEME

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template,request
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+            return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
