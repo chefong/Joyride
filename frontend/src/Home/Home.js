e64ad604812080dd740d7821be1f7b474211c80c
jshin029 <jshin029@ucr.edu>
2019-04-27 19:47:06 -0700

    11th

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -3,7 +3,17 @@ import MapContainer from './MapContainer'
 import Field from './Field'
 import './Home.css'
 
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
+
+
 export default class Home extends Component {
+	
+
+
   state = {
     startAddress: "",
     endAddress: "",
@@ -123,5 +133,52 @@ export default class Home extends Component {
         </div>
       </div>
     )
+  }
+}
+
+class LocationSearchInput extends React.Component {
+
+
+  render() {
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
   }
 }
